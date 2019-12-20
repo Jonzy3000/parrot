cc017eb95f72fc9552f6654822f7a96461365f80
Matt Jones <matt@boclips.com>
2019-12-20 15:52:20 +0000

    Install redux packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Grommet } from "grommet";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 
 const theme = {
   global: {
@@ -11,22 +13,26 @@ const theme = {
   }
 };
 
+const store = configureStore({ reducer: {} });
+
 const App: React.FC = () => {
   return (
     <Grommet theme={theme}>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </Provider>
     </Grommet>
   );
 };
